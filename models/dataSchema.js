792323020077ca2eb39352c23ab2ba0a3a77bd28
Azuka Olisemelie <johntwice8@gmail.com>
2021-05-09 20:24:32 +0100

    New Updates

--- models/dataSchema.js
+++ models/dataSchema.js
@@ -1,10 +1,10 @@
 const {Schema, model} = require('mongoose')
 
 const dataSchema = new Schema({
-    firstname: {type: String, required: true},
-    lastname: {type: String, required: true},
-    email: {type: String, required: true},
-    country: {type: String, required: true}
+    firstname:String,
+    lastname: String,
+    email: String,
+    country: String
 })
 
 exports.data = model("Contact", dataSchema)
